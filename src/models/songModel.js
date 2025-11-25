250ca9c4a5f43ad993d65d7c8067fa8eca029ff2
Yandaveeee <ianpunayo09.com>
2025-11-26 06:40:19 +0800

    update this code
    Signed-off-by: Yandaveeee <ianpunayo09.com>

--- src/models/songModel.js
+++ src/models/songModel.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const songSchema = new mongoose.Schema({
-    name: {type : String, required: true},
+    name: { type: String, required: true },
     desc: { type: String, required: true },
     album: { type: String, required: true },
     image: { type: String, required: true },
     file: { type: String, required: true },
-    duration: {  type: String, required: true },
-    lyrics: { type: String, default: "" },
-    likes: { type: Number, default: 0 }
-})
+    duration: { type: String, required: true },
+    lyrics: { type: String, required: false },
+    video: { type: String, required: false } // Add this line
+});
 
-const songModel = mongoose.models.song || mongoose.model("song",songSchema);
+const songModel = mongoose.models.song || mongoose.model("song", songSchema);
 
 export default songModel;
--- src/routes/songRoute.js
+++ src/routes/songRoute.js
@@ -1,16 +1,17 @@
 import express from 'express';
-import { addSong, listSong, removeSong, updateLyrics } from '../controllers/songController.js';
-import upload from '../middleware/multer.js';
+import { addSong, listSong, removeSong, updateLyrics } from '../controllers/songController.js'
+import upload from '../middleware/multer.js'
 
 const songRouter = express.Router();
 
 songRouter.post('/add', upload.fields([
-    { name: 'image', maxCount: 1 },
-    { name: 'audio', maxCount: 1 }
+    { name: 'image', maxCount: 1 }, 
+    { name: 'audio', maxCount: 1 },
+    { name: 'video', maxCount: 1 }  // Add this line
 ]), addSong);
 
 songRouter.get('/list', listSong);
 songRouter.post('/remove', removeSong);
-songRouter.post('/update-lyrics', updateLyrics); // New route for updating lyrics
+songRouter.post('/update-lyrics', updateLyrics);
 
 export default songRouter;
